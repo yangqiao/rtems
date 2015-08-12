808c4d5e1664c76b0167b5c48ed7702684c593a5
YANG Qiao <yangqiao0505@me.com>
2015-08-12 23:23:50 +0200

    add fbcons support for rpi bsp

--- c/src/lib/libbsp/arm/raspberrypi/include/bsp.h
+++ c/src/lib/libbsp/arm/raspberrypi/include/bsp.h
@@ -40,6 +40,9 @@ extern "C" {
 #define BSP_GPIO_PINS_PER_BANK 32
 #define BSP_GPIO_PINS_PER_SELECT_BANK 10
 
+#define BSP_CONSOLE_UART0   0
+#define BSP_CONSOLE_FB      1
+
 void rpi_init_cmdline(void);
 const char* rpi_cmdline(void);
 const char* rpi_cmdline_arg(const char* arg);
